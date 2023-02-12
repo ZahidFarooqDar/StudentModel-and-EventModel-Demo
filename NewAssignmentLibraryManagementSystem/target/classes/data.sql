2f191e6cdd4461d22d4c964d389dbff35bc73916
ZahidFarooqDar <79592620+ZahidFarooqDar@users.noreply.github.com>
2023-02-12 18:31:03 +0530

    Add files via upload

---
+++ NewAssignmentLibraryManagementSystem/target/classes/data.sql
@@ -0,0 +1,15 @@
+insert into Student(first_name, last_name, age, department)
+VALUES('Zahid', 'Dar', 23, 'CSE'),
+      ('Aamir', 'Mir', 22, 'CSE'),
+      ('Syed', 'Asrar', 24, 'CSE'),
+      ('Bilal', 'Lone', 23, 'CSE'),
+      ('Shazu', 'Akhter', 21, 'CSE');
+
+insert into EVENT_MANAGEMENT(event_name, location_of_event, eventdate, start_time, end_time)
+VALUES('Social-Activity', 'Mumbai', '11Feb2023', '4:00pm', '5:00pm'),
+      ('Sports-Event', 'Delhi', '12Feb2023', '1:00pm', '6:00pm'),
+      ('Public-Awareness', 'Delhi', '13Feb2023', '1:00pm', '9:00pm'),
+      ('Workshop', 'Kashmir', '14Feb2023', '3:00pm', '5:00pm'),
+      ('Bharat-Jodo', 'Kashmir', '15Feb2023', '1:00pm', '11:00pm');
+
+
